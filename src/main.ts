2c0d11c1278bc2a74aa98504cbcf570349f8eb43
Deric Cain <deric.cain@gmail.com>
2022-09-12 05:51:11 +0000

    Add sentry

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import {
   faCircleExclamation,
   faPaw
 } from '@fortawesome/free-solid-svg-icons'
+import { BrowserTracing } from '@sentry/tracing'
+import * as Sentry from '@sentry/vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from '@/App.vue'
@@ -31,7 +33,20 @@ library.add(
 )
 
 const app = createApp(App)
-  .use(router)
-  .use(pinia)
+
+
+Sentry.init({
+  app,
+  dsn: 'https://5c3a8e528ba34a409de120a8fe6cdf3e@o1406124.ingest.sentry.io/6739572',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['localhost', /./, /dog.ceo./, /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+})
+
+app.use(router).use(pinia)
 
 app.mount('#app')
